347ab727dbf3fe41f67f3ab2bf860e3d72ae79fe
ThomasBlake777 <Thomas_Blake7@proton.me>
2025-12-18 13:21:45 +0200

    feat: add AC01 to auto-blacklist chargeback codes
    Added AC01 (Incorrect Account Number) to blacklist_codes in config/tether.php
    Full list: AC01, AC04, AC06, AG01, MD01

--- config/tether.php
+++ config/tether.php
@@ -6,6 +6,6 @@ return [
     'chargeback' => [
         'alert_threshold' => env('CB_ALERT_THRESHOLD', 25),
         'cache_ttl' => env('CB_CACHE_TTL', 900),
-        'blacklist_codes' => ['AC04', 'AC06', 'AG01', 'MD01'],
+        'blacklist_codes' => ['AC01', 'AC04', 'AC06', 'AG01', 'MD01'],
     ],
 ];
