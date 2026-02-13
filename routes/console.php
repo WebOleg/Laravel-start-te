9983e61505c14634c46ea06b4ba56d7bc325cab9
dev the new <thebsabdev@proton.me>
2026-02-13 20:52:55 +0500

    fix: cron job set to run at every two hours for CB codes

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 Schedule::command('billing:dispatch')->everyMinute();
-Schedule::command('emp:fetch-chargeback-code --empty --chunk=300')->everySixHours();
+Schedule::command('emp:fetch-chargeback-codes --empty --chunk=200')->everyTwoHours();
 Schedule::command('emp:sync-chargebacks --days=1')->dailyAt('06:00');
 // Cleanup broken batches (negative pending_jobs) every hour
 Schedule::command('batches:cleanup')->hourly();
