0b84b83820840aed2cf99c6cd65599c5f7f4b741
ThomasBlake777 <Thomas_Blake7@proton.me>
2026-02-14 17:25:10 +0200

    fix(config): change CB alert_threshold fallback from 25 to 30

--- config/tether.php
+++ config/tether.php
@@ -4,7 +4,7 @@
  */
 return [
     'chargeback' => [
-        'alert_threshold' => env('CB_ALERT_THRESHOLD', 25),
+        'alert_threshold' => env('CB_ALERT_THRESHOLD', 30),
         'cache_ttl' => env('CB_CACHE_TTL', 900),
         'blacklist_codes' => ['AC01', 'AC04', 'AC06', 'AG01', 'MD01'],
         'excluded_cb_reason_codes' => ['XT33', 'XT73'],
